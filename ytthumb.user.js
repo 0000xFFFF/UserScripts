bb3c3ce3971c2b6ebfef532dcd557cfd7b2c6735
0000xFFFF <coldmailgg@gmail.com>
2024-09-08 00:56:32 +0200

    new ytthumb script

--- ytthumb.user.js
+++ ytthumb.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Youtube Thumbnail
+// @name         YouTube Thumbnail Button
 // @namespace    UserScript
 // @version      1.0
-// @description  Adds a Button to the Current Thumbnail of the Video.
-// @author       0xC0LD
+// @description  Adds a button to the current thumbnail of the video
+// @author       0000xFFFF
 // @include      http*://*.youtube.com/*
 // @include      http*://youtube.com/*
 // @include      http*://*.youtu.be/*
@@ -12,87 +12,70 @@
 // @icon         https://www.youtube.com/favicon.ico
 // ==/UserScript==
 
-var page = "";
-var index = 0;
-function get() {
-	// remove priv buttons
-	let btns = document.getElementById("btn_thumbnail_url");
-	if (btns != null) { btns.remove(); }
-	
-	// get id
-	let split = window.location.search.split('v=');
-	if (split.length != 2) { return; }
-	let video_id = split[1];
-	let ampersandPosition = video_id.indexOf('&');
-	if (ampersandPosition != -1) { video_id = video_id.substring(0, ampersandPosition); }
-	
-	// make urls
-	let imgD = `https://img.youtube.com/vi/${video_id}/maxresdefault.jpg`; // Maximum Resolution
-	let imgH = `https://img.youtube.com/vi/${video_id}/hqdefault.jpg`;     // High Quality
-	let imgM = `https://img.youtube.com/vi/${video_id}/mqdefault.jpg`;     // Medium Quality
-	let imgL = `https://img.youtube.com/vi/${video_id}/sddefault.jpg`;     // Low Quality
-	let urls = [ imgD, imgH, imgM, imgL ];
-	
-	// make requests
-	if (index > urls.length - 1) { index = 0; }
-	httpGetAsync(urls[index]);
-	index++;
-}
+(function() {
+    const buttonId = "btn_thumbnail_url";
+    const checkInterval = 500; // Interval for checking element existence
 
-function httpGetAsync(theUrl) {
-	console.log("... : " + theUrl);
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function() { 
-	if (xmlHttp.readyState != 4) { return; }
-		switch(xmlHttp.status) {
-			case 200: console.log("202 : " + theUrl); createButton(theUrl); break;
-			case 404: console.log("404 : " + theUrl); get(); break;
-		}
-	}
-	xmlHttp.open("GET", theUrl, true);
-	
-	try { xmlHttp.send(null); }
-	catch (err) {
-		console.log(err);
-		get();
-	}
-}
+    const intervalId = setInterval(() => {
+        const startElement = document.querySelector("#start");
+        if (startElement) {
+            clearInterval(intervalId); // Stop checking
 
+            const params = new URLSearchParams(window.location.search);
+            const videoId = params.get("v");
+            if (videoId) {
+                const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
 
-var style = `
-	float: right !important;
-	color: rgba(255, 255, 255, 0.54) !important;
-	background: transparent !important;
-	font-size: 11px;
-	border: solid rgba(255, 255, 255, 0.54) 1px !important;
-	padding: 3px !important;
-	text-decoration: none !important;
-	text-transform: uppercase !important;
-`;
+                fetch(thumbnailUrl)
+                    .then(response => {
+                        if (response.ok) {
+                            let existingButton = document.getElementById(buttonId);
+                            if (existingButton) {
+                                existingButton.remove();
+                            }
 
-function createButton(url) {
-	let btn = document.createElement("a");
-	btn.style = style;
-	btn.id = "btn_thumbnail_url";
-	btn.innerHTML = "Thumbnail";
-	btn.href = url;
-	btn.title = url.split('/').pop() + " (" + url + ")";
-	
-	let meta = document.getElementById("meta");
-	meta.appendChild(btn);
-	console.log("BTN : " + url);
-	
-	if (!document.getElementById(btn.id)) { setTimeout(function() { createButton(url) }, 100); }
-}
+                            const button = document.createElement("a");
+                            button.id = buttonId;
+                            button.href = thumbnailUrl;
+                            button.title = "Max Resolution Thumbnail";
+                            button.style.cssText = `
+                                display: inline-flex;
+                                align-items: center;
+                                margin-top: 10px;
+                                padding: 5px 10px;
+                                color: #ffffff;
+                                background-color: #cc0000;
+                                border-radius: 4px;
+                                text-decoration: none;
+                                font-weight: bold;
+                                font-size: 14px;
+                            `;
 
+                            // Create and style the image element
+                            const img = document.createElement("img");
+                            img.src = thumbnailUrl;
+                            img.alt = "Thumbnail";
+                            img.style.cssText = `
+                                width: 30px; /* Adjust width as needed */
+                                height: auto;
+                                max-height: 30px; /* Ensure the height fits within the button */
+                                margin-right: 5px;
+                                border-radius: 4px;
+                                object-fit: contain; /* Maintain aspect ratio */
+                            `;
 
-function checker() {
-	if (page != window.location.href) {
-		page = window.location.href;
-		index = 0;
-		get();
-	}
-}
+                            // Append image and text to the button
+                            button.appendChild(img);
+                            button.appendChild(document.createTextNode(" Maxres Thumbnail"));
 
-setInterval(checker, 500);
+                            startElement.appendChild(button);
+                        } else {
+                            console.error("Thumbnail not found.");
+                        }
+                    })
+                    .catch(error => console.error("Fetch error: ", error));
+            }
+        }
+    }, checkInterval);
+})();
 
